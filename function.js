36de8c64bbb14af56d83baaa1d0a67738c14b60f
alexclaureM <82347863+alex-claure-m@users.noreply.github.com>
2024-08-22 15:22:04 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,21 +1,19 @@
 (function() {
     
     var gistBloggerPath = "https://cdn.jsdelivr.net/gh/alex-claure-m/sparkle@main/sparkle.js";
-
-    // Initialize the load the main script.
-    try {
-        initMiScript();
-    } catch(e) {
-        var scriptId = 'gistPrinter';
-        if (document.getElementById(scriptId) === null) {
-            var elem = document.createElement('SCRIPT');
-            elem.id = scriptId;
-            elem.onload = function() {
-              initMiScript();
-            }
-            elem.src = gistBloggerPath;
-            var theBody = document.getElementsByTagName('body')[0];
-            theBody.appendChild(elem);
-        }
-    }
+      try {
+             initMiScript();
+           } catch(e) {
+            var scriptId = &#39;miScript&#39;;
+            if (document.getElementById(scriptId) === null) {
+                 var elem = document.createElement(&#39;SCRIPT&#39;);
+                 elem.id = scriptId;
+                  elem.onload = function() {
+                    initMiScript(); // Llama la función después de que el script se cargue
+                  };
+                  elem.src = miScriptURL; // Cambia esto a la URL de tu script externo si lo tienes en uno
+                   var theBody = document.getElementsByTagName(&#39;body&#39;)[0];
+                   theBody.appendChild(elem);
+             }
+          }
 })();
